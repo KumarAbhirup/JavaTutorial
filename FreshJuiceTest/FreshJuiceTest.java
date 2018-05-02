802ff3ee8dbe102634d036e56f32527d353f804c
Kumar Abhirup <kumar@iqubex.com>
2018-05-02 13:28:51 +0530

    Fresh Juice Tutorial Added
    This is a tutorial where two classes are used with an ENUM.

---
+++ FreshJuiceTest/FreshJuiceTest.java
@@ -0,0 +1,16 @@
+class FreshJuice {
+   int SMALL = 10;
+   int MEDIUM = 15;
+   int LARGE = 20;
+   enum FreshJuiceSize{ SMALL, MEDIUM, LARGE }
+   FreshJuiceSize size;
+}
+
+public class FreshJuiceTest {
+
+   public static void main(String args[]) {
+      FreshJuice juice = new FreshJuice();
+      juice.size = FreshJuice.FreshJuiceSize.MEDIUM ;
+      System.out.println("Size: " + juice.MEDIUM);
+   }
+}
